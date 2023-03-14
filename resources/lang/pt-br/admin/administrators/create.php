5da66181cf7b37b6c268ae0654c514f6e6044b90
Carlos Alexandre <carlos24106@outlook.com>
2023-03-14 10:56:52 -0300

    Traduzindo para o pt-br

---
+++ resources/lang/pt-br/admin/administrators/create.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'title' => 'Criar administrador',
+    'title_card' => 'Adicionar informações do admin',
+    'name' => 'Nome',
+    'email' => 'E-mail',
+    'password' => 'Senha',
+    'password_confirm' => 'Confirmar senha',
+    'help_info' => 'Outras informações como foto, descrição, etc. podem ser alteradas após o login.',
+    'button' => 'Criar',
+
+    'user_create' => 'Usuário criado com sucesso',
+];
---
+++ resources/lang/pt-br/admin/administrators/index.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+
+    'title' => 'Lista de administradores',
+];
